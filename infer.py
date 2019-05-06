128aacdd0c1e28cce8e005aa17d5b1c7a290c403
baiyuxiu <1525645606@qq.cpm>
2019-05-06 10:24:31 +0800

    infer_ssd300_scipts

--- infer.py
+++ infer.py
@@ -5,8 +5,8 @@ import cv2
 from ssd import build_ssd
 
 num_classes = 81
-image = cv2.imread("data/1.jpg")
-weights = "weights/ssd300_COCO_10000.pth"
+image_path = "data/1.jpg"
+weights = "weights/ssd300_COCO_140000.pth"
 
 #cv2.imshow("fafda", image)
 #cv2.waitKey()
@@ -17,6 +17,7 @@ def get_features_hook(self, input, output):
 
 if __name__ == '__main__':
     net = build_ssd('test', 300, num_classes)
+    image = cv2.imread(image_path)
     image = cv2.resize(image, (300, 300))
     image = torch.Tensor(image)
     image = image.permute(2, 0, 1)
@@ -24,6 +25,15 @@ if __name__ == '__main__':
 #load weights to the net
     net.load_state_dict(torch.load(weights))
     output = net(image)
+#begin our painting
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (300, 300))
+    for i in range(len(output)):
+        if output[i][5] > 0.3:
+          cv2.rectangle(image, (int(output[i][0] * 300), int(output[i][1] * 300)),\
+             (int(output[i][2] * 300), int(output[i][3] * 300)), (255, 0, 0), 2)
+    cv2.imshow(image_path, image)
+    cv2.waitKey()
     print(output.shape)
 #get the specific layer value
 
